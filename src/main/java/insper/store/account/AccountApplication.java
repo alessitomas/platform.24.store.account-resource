268b3bde86c3d2c6082beb47841237146d7aca4b
Alessi181 <tomasalessi@gmail.com>
2024-05-26 21:33:50 -0300

    Remove Redis

--- src/main/java/insper/store/account/AccountApplication.java
+++ src/main/java/insper/store/account/AccountApplication.java
@@ -2,7 +2,6 @@ package insper.store.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
@@ -11,7 +10,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
-@EnableCaching
+
 public class AccountApplication {
 
     public static void main(String[] args) {
